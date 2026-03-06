e9340fef56f30a6c3075fed20cd5ba5900dfd4e1
Mercy-line <mercylinenyaboke08@gmail.com>
2026-03-06 19:43:00 +0300

    remove comments

--- app/users/feature/restaurant/components/Overlay.tsx
+++ app/users/feature/restaurant/components/Overlay.tsx
@@ -1,15 +1,3 @@
-// interface OverlayProps {
-//   onClose: () => void;
-// }
-
-// export default function Overlay({ onClose }: OverlayProps) {
-//   return (
-//     <div
-//       onClick={onClose}
-//       className="fixed inset-0 bg-black/40 backdrop-blur-sm z-40 transition-opacity duration-300"
-//     />
-//   );
-// }
 
 interface OverlayProps {
   onClose: () => void;
